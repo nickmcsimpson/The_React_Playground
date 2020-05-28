f45e04072e906056e31a97577981348a753799db
nickmcsimpson <n.mcsimp@gmail.com>
2020-05-28 18:55:41 -0500

    remove gitignore files

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,24 +1,19 @@
-import { React, Component } from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Home from './App/pages/Home';
 import Planets from './App/pages/Planets';
+import Spotify from './App/pages/Spotify';
 
 class App extends Component {
   render() {
-    const App = () => (
-      <div>
+    return (
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/planets' component={Planets}/>
+          <Route path='/spotify' component={Spotify} />
         </Switch>
-      </div>
-    )
-    return (
-      <Switch>
-        <App/>
-      </Switch>
     );
   }
 }
